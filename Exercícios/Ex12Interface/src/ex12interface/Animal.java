79b6c141e1e0642db1c4d374b38f9ef5a8451e73
Anderson <andersonmarcolino@gmail.com>
2019-09-25 10:21:57 -0300

    Atualizando Exemplo de Interface

--- Exercícios/Ex12Interface/src/ex12interface/Animal.java
+++ Exercícios/Ex12Interface/src/ex12interface/Animal.java
@@ -3,7 +3,7 @@ package ex12interface;
 
 
 public interface Animal {
-    String planeta = "Terra"; //Se torna um atributo global
+    String planeta = "Terra"; //Se torna um atributo global - Mas, qual seria o propsóito de um atributo (neste caso campo), se não irá instanciar um objeto?
     
     //Os métodos não podem ser privados quando declarados em interfaces (o que faz todo o sentido)
     public void fazerBarulo();
